1e85047f304da6726e091fb1f05474ae092974f7
B Vedant Pandey <bvedantp@gmail.com>
2021-07-22 17:34:39 +0530

    Made the main gameplay function

--- logic.js
+++ logic.js
@@ -5,7 +5,43 @@ function computerPlay() {
     } else if (randomNumber >3 && randomNumber <=6) {
         return ('paper');
     } else {
-        return ('scisssors');
+        return ('scissors');
     }
 }
-console.log(computerPlay());
+let cSelect = computerPlay();
+console.log(cSelect); //to see what computer selects
+let pSelect = prompt('Enter your selection: ');
+
+function gameStart(computerSelection, playerSelection) {
+    if(computerSelection === playerSelection) {
+        console.log('We have a DRAW people!!');
+        return;
+    }
+    switch (playerSelection) {
+        case 'rock' :
+            if(computerSelection == 'paper') {
+                console.log(`You LOSE! ${computerSelection} beats ${playerSelection}`);
+            } else {
+                console.log(`You WIN! ${playerSelection} beats ${computerSelection}`);
+            } 
+            break;
+
+        case 'scissors' :
+            if(computerSelection == 'rock') {
+                console.log(`You LOSE! ${computerSelection} beats ${playerSelection}`);
+            } else {
+                console.log(`You WIN! ${playerSelection} beats ${computerSelection}`);
+            }
+            break;
+
+        case 'paper' :
+            if(computerSelection== 'scissors') {
+                console.log(`You LOSE! ${computerSelection} beats ${playerSelection}`);
+            } else {
+                console.log(`You WIN! ${playerSelection} beats ${computerSelection}`);
+            }
+            break;
+    }
+}
+
+gameStart(cSelect,pSelect);
